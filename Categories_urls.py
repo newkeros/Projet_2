673a326cb4156fd43799337a9a94e5734bbb91d6
laetitia gigant <albert.greg@gmail.com>
2021-01-04 20:43:55 +0100

    List name modification

--- Categories_urls.py
+++ Categories_urls.py
@@ -5,7 +5,7 @@ import requests
 def collect_categories_urls(website_url):
     response = requests.get(website_url)
     soup = BeautifulSoup(response.text, "lxml")
-    mylist = list()
+    links_list = list()
     for links in soup.findAll("a")[3:53]:
-        mylist.append("http://books.toscrape.com/" + links.get("href"))
-    return mylist
+        links_list.append("http://books.toscrape.com/" + links.get("href"))
+    return links_list
